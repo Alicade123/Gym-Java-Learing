d32bfd3e255ed0cbd39c4668e142e260c026e5d9
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-04 21:53:44 +0200

    Second update made on the static keyword file

--- src/StaticKeyword.java
+++ src/StaticKeyword.java
@@ -1,13 +1,13 @@
 public class StaticKeyword{
     public static void main (String[] args){
 
-        StaticMembers friend1 = new StaticMembers();
-        StaticMembers friend2 = new StaticMembers();
-        StaticMembers friend3 = new StaticMembers();
+        _2StaticMembers friend1 = new _2StaticMembers();
+        _2StaticMembers friend2 = new _2StaticMembers();
+        _2StaticMembers friend3 = new _2StaticMembers();
         friend1.setFriend("Arcade");
         friend1.setFriend("Latifa");
         friend2.setFriend("Zed");
 //    System.out.println(StaticMembers.numberOfFriends);
-        StaticMembers.getNumberOfFriends();
+        _2StaticMembers.getNumberOfFriends();
     }
 }
