e3fe48530e0f63a491f3522c80bdef5ce83fc493
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-06 21:37:01 +0200

    The learning progress

---
+++ src/_9WrapperClasses.java
@@ -0,0 +1,23 @@
+
+
+public class _9WrapperClasses {
+    public static void main(String[] args) {
+        // Primitive Datatypes  // Wrapper Classes
+                                //Autoboxing                //Unboxing
+        byte a = 1;             Byte a1 =1;                 byte a2 = a1;
+        short b = 1;            Short b1 =1;                short b2 = b1;
+        int c= 1;               Integer c1 =1;              int c2 = c1;
+        long d= 1L;             Long d1 =1L;                long d2 = d1;
+        float ee= 1F;           Float ee1 = 1F;             float ee2 = ee1;
+        double e = 2.12;        Double e1 = 1D;             double e2 = e1;
+        char f = 'a';           Character f1 ='a';          char f2 = f1;
+        boolean g = true;       Boolean g1 =true;           boolean g2 = g1;
+
+
+
+
+
+
+
+    }
+}
