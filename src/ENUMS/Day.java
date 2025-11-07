b6429329a677ab61f9929f7490ef20400977ecd8
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-07 11:19:23 +0200

    The Java Enums learning progress

---
+++ src/ENUMS/Day.java
@@ -0,0 +1,22 @@
+package ENUMS;
+
+public enum Day {
+    SUNDAY(1),
+    MONDAY(2),
+    TUESDAY(3),
+    WEDNESDAY(4),
+    THURSDAY(5),
+    FRIDAY(6),
+    SATURDAY(7),
+    PIZZADAY(8);
+    private final int numberDay;
+    Day(int numberDay) {
+        this.numberDay = numberDay;
+    }
+
+    public int getNumberDay() {
+        return numberDay;
+    }
+
+
+}
