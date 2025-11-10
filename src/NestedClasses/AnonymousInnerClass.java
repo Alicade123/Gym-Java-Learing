60f93799213a9e6c35a90e3f72b56a7408b2fbad
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-10 21:32:24 +0200

    Learning the Anonymous class and nested classes

--- src/NestedClasses/AnonymousInnerClass.java
+++ src/NestedClasses/AnonymousInnerClass.java
@@ -1,4 +1,14 @@
 package NestedClasses;
 
 public class AnonymousInnerClass {
+    /*Anonymous class: inner class without name.
+                        Only single object is created form one.
+                        The object might have extra or changes and no need to create separate inner classes when it needs it only once
+
+                        They look identical to constructors but they have class definition
+                        They are also great for LISTENERS
+
+    */
+
+
 }
