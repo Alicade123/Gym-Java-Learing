a1e5b451290d8a5d3bda58b2edee621a44abe1ac
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-13 19:19:34 +0200

    Practicing the OOP part in Java (Polymorphism, Inhertance and encapsulation)

--- src/PracticalMental/Shapes/MainShape.java
+++ src/PracticalMental/Shapes/MainShape.java
@@ -8,11 +8,11 @@ public class MainShape {
         Shape shape = new Shape();
         Circle circle = new Circle(3);
         Rectangle rectangle = new Rectangle(3,9);
-/*
+
         System.out.println("Area of the shape is "+shape.area());
         System.out.println("Area of the circle is "+circle.area());
         System.out.println("Area of the circle is "+rectangle.area());
- */
+
     Shape [] shapes = {shape, circle, rectangle};
         for(Shape shp:shapes){
         System.out.println("The Area is: "+ shp.area());
