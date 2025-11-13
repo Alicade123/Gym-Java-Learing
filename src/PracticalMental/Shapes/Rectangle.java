a6ab5aebedc8bd6e4765ce547f0c9e35d079ce0b
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-13 19:19:47 +0200

    Practicing the OOP part in Java (Polymorphism, Inhertance and encapsulation)

--- src/PracticalMental/Shapes/Rectangle.java
+++ src/PracticalMental/Shapes/Rectangle.java
@@ -1,12 +1,14 @@
 package PracticalMental.Shapes;
 
 public class Rectangle extends Shape{
+
     private double length;
     private double width;
     public Rectangle(double length, double width){
         this.length = length;
         this.width= width;
     }
+
         @Override
         public double area(){
             return length*width;
