14655af28bca4c4acb87ab28623f5d089ee32ddc
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-13 14:47:54 +0200

    Java learning progress about the Threading

--- src/Threading/MainThread.java
+++ src/Threading/MainThread.java
@@ -1,4 +1,20 @@
 package Threading;
 
+import java.util.Scanner;
+
 public class MainThread {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.setDaemon(true); // To finish our program while main thread finishes execution
+        thread.start();
+
+        System.out.println("Please!, You have 5 Seconds to enter your name");
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is: "+name);
+
+    }
 }
--- src/Threading/MyRunnable.java
+++ src/Threading/MyRunnable.java
@@ -1,4 +1,19 @@
 package Threading;
 
-public class MyRunnable {
+public class MyRunnable implements Runnable {
+    @Override
+    public void run(){
+        for(int i =1 ; i<=5; i++){
+            try{
+                Thread.sleep(10000);
+            }catch(InterruptedException e){
+                System.out.println("The thread was interrupted");
+            }
+            if(i==5){
+                System.out.println();
+                System.out.println("Thank you, Time is up Kid!");
+                System.exit(0); // To exit the program as well as the time is up.
+            }
+        }
+    }
 }
